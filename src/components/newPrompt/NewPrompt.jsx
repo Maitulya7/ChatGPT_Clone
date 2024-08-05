735ce82d619254ee876a6fe25dc8acdb2023315f
maitulya7 <vaghelamaitulya85@gmail.com>
2024-08-05 12:38:15 +0530

    ChatPage

---
+++ src/components/newPrompt/NewPrompt.jsx
@@ -0,0 +1,9 @@
+import './newPrompt.css'
+
+const NewPrompt = () => {
+  return (
+    <div className='newPrompt'>NewPrompt</div>
+  )
+}
+
+export default NewPrompt
--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,9 +1,70 @@
-import './chatPage.css'
+import { useRef } from "react";
+import "./chatPage.css";
+import { useEffect } from "react";
+import NewPrompt from "../../components/newPrompt/NewPrompt";
 
 const ChatPage = () => {
-  return (
-    <div className='chatPage'>ChatPage</div>
-  )
-}
+  const endRef = useRef(null);
 
-export default ChatPage
+  useEffect(() => {
+    endRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
+  return (
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Text Message from AI</div>
+          <div className="message user">
+            Text Message from User Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. Ut, minima facere voluptate distinctio quidem
+            praesentium nostrum consectetur perferendis enim eligendi.
+          </div>
+          <div className="message">Text Message from AI</div>
+          <div className="message user">Text Message from User</div>
+          <div className="message">
+            Text Message from AI Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Quae, reprehenderit.
+          </div>
+          <div className="message user">Text Message from User</div>
+          <div className="message">Text Message from AI</div>
+          <div className="message user">Text Message from User</div>
+          <div className="message">Text Message from AI</div>
+          <div className="message user">
+            Text Message from User Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. Ut, minima facere voluptate distinctio quidem
+            praesentium nostrum consectetur perferendis enim eligendi.
+          </div>
+          <div className="message">Text Message from AI</div>
+          <div className="message user">Text Message from User</div>
+          <div className="message">
+            Text Message from AI Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Quae, reprehenderit.
+          </div>
+          <div className="message user">Text Message from User</div>
+          <div className="message">Text Message from AI</div>
+          <div className="message user">Text Message from User</div>
+          <div className="message">Text Message from AI</div>
+          <div className="message user">
+            Text Message from User Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. Ut, minima facere voluptate distinctio quidem
+            praesentium nostrum consectetur perferendis enim eligendi.
+          </div>
+          <div className="message">Text Message from AI</div>
+          <div className="message user">Text Message from User</div>
+          <div className="message">
+            Text Message from AI Lorem, ipsum dolor sit amet consectetur
+            adipisicing elit. Quae, reprehenderit.
+          </div>
+          <div className="message user">Text Message from User</div>
+          <div className="message">Text Message from AI</div>
+          <div className="message user">Text Message from User</div>
+          <NewPrompt />
+          <div ref={endRef} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
